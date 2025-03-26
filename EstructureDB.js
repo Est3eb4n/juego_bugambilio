d206b8a64143105b289fa6944981bec4c957ff70
Est3eb4n <119062636+Est3eb4n@users.noreply.github.com>
2025-03-26 18:20:05 -0500

    Add files via upload

---
+++ EstructureDB.js
@@ -0,0 +1,110 @@
+// Estructura de datos de recursos
+
+[
+    {
+        name:"comida",
+        tiempoRecoleccion:1, 
+        cantidadRecoleccion:2
+    },
+    {
+        name:"madera",
+        tiempoRecoleccion: 1,
+        cantidadRecoleccion: 2,
+    },
+    {
+        name:"piedra",
+        tiempoRecoleccion: 1,
+        cantidadRecoleccion: 2,
+    }
+],
+
+// Estructura de datos Arbol de tecnologia Edad 0
+
+[
+    {
+        "recoleccion":[
+                        {
+                            name:"canasta",
+                            precioMejora: 80, // $
+                            mejoraRecoleccion: 120, // unid
+                            tiempoRecoleccion: 60, // seg
+                            timepoInvestigacion: 180 // seg
+                        },
+                        {
+                            name:"Pesca",
+                            precioMejora: 20, // $
+                            mejoraRecoleccion: 6, // unid
+                            tiempoRecoleccion: 6, // seg
+                            timepoInvestigacion: 240 // seg
+                        }
+                      ]
+    },
+    {
+        "herramientas":
+                      [
+                        {
+                            name:"Pico de piedra",
+                            precioMejora: 60, // $
+                            mejoraRecoleccion: 4, // unid
+                            tiempoRecoleccion: 1, // seg
+                            timepoInvestigacion: 60 // seg
+                        },
+                        {
+                            name:"Azada de piedra",
+                            precioMejora: 70, // $
+                            mejoraRecoleccion: 4, // unid
+                            tiempoRecoleccion: 1, // seg
+                            timepoInvestigacion: 60 // seg
+                        },
+                        {
+                            name:"Hacha de piedra",
+                            precioMejora: 60, // $
+                            mejoraRecoleccion: 20, // unid
+                            tiempoRecoleccion: 240, // seg
+                            timepoInvestigacion: 60 // seg
+                        }
+                      ]
+    },
+// Estructura de datos Arbol de tecnologia Edad 0
+    {
+        "domesticacion":
+                        [
+                            {
+                                name:"Animales de trabajo",
+                                precioMejora: 100,
+                                timepoInvestigacion: 180
+                            }
+                        ],
+        "recoleccion":
+                    [
+                        {
+                            name:"Siembra",
+                            precioMejora: 100,
+                            mejoraRecoleccion: 40,
+                            tiempoRecoleccion: 6,
+                            timepoInvestigacion: 180
+                        },
+                        {
+                            name:"Riego rustico",
+                            precioMejora: 100,
+                            mejoraRecoleccion: 40,
+                            tiempoRecoleccion: 6,
+                            timepoInvestigacion: 180
+                        },
+                        {
+                            name:"Granja I",
+                            precioMejora: 150,
+                            mejoraRecoleccion: 40,
+                            tiempoRecoleccion: 6,
+                            timepoInvestigacion: 200
+                        },
+                        {
+                            name:"Carreta",
+                            precioMejora: 100,
+                            mejoraRecoleccion: 40,
+                            tiempoRecoleccion: 6,
+                            timepoInvestigacion: 180
+                        }
+                    ]
+    }
+]
